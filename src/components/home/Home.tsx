bb58429f7c3adaa4438b896fa8e00dcbf6b10253
TomDijkema <tom.dijkema@naturalis.nl>
2024-03-29 16:56:50 +0100

    Add Header and Footer components
    Adds Header and Footer components

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,27 @@
+/* Import Dependencies */
+import { Container, Row, Col } from 'react-bootstrap';
+
+/* Import Components */
+import Header from "components/general/header/Header";
+import Footer from 'components/general/footer/Footer';
+
+
 /** Base component that renders the Home page */
 const Home = () => {
     return (
-        <> </>
+        <div className="h-100 d-flex flex-column">
+            <Header />
+
+            <Container fluid className="flex-grow-1">
+                <Row className="h-100">
+                    <Col lg={{ span: 10, offset: 1 }}>
+
+                    </Col>
+                </Row>
+            </Container>
+
+            <Footer />
+        </div>
     );
 }
 
