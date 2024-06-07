2fa527a47dca1990859d39e392117f14d8e28ca5
TomDijkema <tom.dijkema@naturalis.nl>
2024-06-07 10:39:09 +0200

    Sonar commit 4
    Fourth commit for resolving Sonar Cloud issues

--- src/components/general/header/Header.tsx
+++ src/components/general/header/Header.tsx
@@ -1,6 +1,5 @@
 /* Import Dependencies */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 
